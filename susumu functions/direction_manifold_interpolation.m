b9e19d23c84c6cda6abbcebbc37f54fe33fd636e
shin091602 <shin.soichiro.103@s.kyushu-u.ac.jp>
2025-04-21 19:30:52 +0900

    feat: wrote script to calculate the manifold orientation of the interpolated points

--- susumu functions/direction_manifold_interpolation.m
+++ susumu functions/direction_manifold_interpolation.m
@@ -29,5 +29,42 @@ T = fin_qpos(d*N*M+1);
 rho = fin_qpos(d*N*M+2);
 
 % fourier matrices
-[FR,~,~] = fun_Fourier_interpolation(rho,p);
+FR = fun_Fourier_interpolation(rho,p);
 
+% get unstabel directions of full torus
+% initialization
+del_w_us_interpolation = zeros(d,num_iter,num_iter);
+for i = 1:num_iter
+    % invariant curve idx
+    k = d*num_iter*(i-1)+1:d*num_iter*i;
+    % invariant circle data
+    X0 = fin_qpos(k);
+    Mo = zeros(d*num_iter,d*num_iter);
+    for j = 1:num_iter
+        Y0 = [X0(d*j-(d-1):d*j); reshape(eye(d),[],1)];
+        % ODE
+        [~,Y] = ode113(@(t,x) fun_stm_cr3bp(t,x,mu),[0 T],Y0,options_ODE);
+        % Monodromy matrix of all states
+        Mo(d*j-(d-1):d*j,d*j-(d-1):d*j) = reshape(Y(end,d+1:d+d^2),d,d);
+    end
+    % lineralization of stroboscopic map
+    Gw = FR*Mo;
+    VD = zeros(d,num_iter);
+    for j = 1:num_iter
+        % eigenvalue and eigenvector
+        [V,D] = eig(Gw(d*j-(d-1):d*j,d*j-(d-1):d*j));
+        % eigenvalue
+        D = diag(D);
+        [~,idmax] = max(abs(D));
+        % eigenfunctions
+        Vd = V(:,idmax);
+        if Vd(1) < 0
+            Vd = -Vd;
+        end
+        VD(:,j) = Vd;
+    end
+    del_w_us_interpolation(:,:,i) = VD;
+end
+
+end
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
