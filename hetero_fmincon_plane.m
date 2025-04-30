21be5e30ea6430983e487a87f819398a276e4dae
shin091602 <shin.soichiro.103@s.kyushu-u.ac.jp>
2025-04-30 14:05:42 +0900

    feat: propagate the manifolds of abtrary point

--- hetero_fmincon_plane.m
+++ hetero_fmincon_plane.m
@@ -86,6 +86,9 @@ p('iteration_max') = 1000;
 p('threshold') = 1e-10;
 p('count_max') = 8000;
 
+% setting for propagation of the manifolds
+p('pert') = 1e-6; % perturbation
+
 %% natural parameter continuation
 delta = 1e-4; % step size
 count = 0; % count of the iteration
@@ -250,7 +253,116 @@ X2_s = Y2(idx2, :);
 % Generate interpolating functions
 interpLyap1 = @(tht_q) cell2mat(arrayfun(@(k) interp1(theta1_s, X1_s(:,k), tht_q, 'pchip'), 1:size(X1_s,2), 'UniformOutput', false) );
 interpLyap2 = @(tht_q) cell2mat(arrayfun(@(k) interp1(theta2_s, X2_s(:,k), tht_q, 'pchip'), 1:size(X2_s,2), 'UniformOutput', false) );
-% use the interpolating functions
-tht_query = linspace(0, 2*pi, 500).';
-X1_resampled = interpLyap1(tht_query);
-X2_resampled = interpLyap2(tht_query);
+
+%% use the interpolating functions
+
+
+% monodromy matrix
+M_1 = reshape(Y1(end, 7:end), 6, 6);
+M_2 = reshape(Y2(end, 7:end), 6, 6);
+% Eigenvectors and eigenvalues analysis
+[V1, D1] = eig(M_1);
+D1 = diag(D1);
+[~, idx_s1] = min(abs(D1)); % stable
+[~, idx_u1] = max(abs(D1)); % unstable
+[V2, D2] = eig(M_2);
+D2 = diag(D2);
+[~, idx_s2] = min(abs(D2)); % stable
+[~, idx_u2] = max(abs(D2)); % unstable
+
+% Stable and unstable eigenvectors
+vector_stable_1 = V1(:, idx_s1);
+if vector_stable_1(1) < 0
+    vector_stable_1 = -vector_stable_1;
+end
+vector_unstable_1 = V1(:, idx_u1);
+if vector_unstable_1(1) < 0
+    vector_unstable_1 = -vector_unstable_1;
+end
+
+vector_stable_2 = V2(:, idx_s2);
+if vector_stable_2(1) < 0
+    vector_stable_2 = -vector_stable_2;
+end
+vector_unstable_2 = V2(:, idx_u2);
+if vector_unstable_2(1) < 0
+    vector_unstable_2 = -vector_unstable_2;
+end
+
+% Apply perturbation to abitrary point of the Lyapunov orbit
+% set the angle of the pointーーーーーーーーーーーーーーーーーーーーーーー
+tht_query_1 = 0;
+tht_query_2 = 0;
+X1_resampled = interpLyap1(tht_query_1);
+X2_resampled = interpLyap2(tht_query_2);
+% Grab state at the fixed point
+x_star_1 = X1_resampled(1:6);
+x_star_2 = X2_resampled(1:6);
+% Grab state transition matrix at the fixed point
+phi_star_1 = reshape(X1_resampled(7:end), 6, 6);
+phi_star_2 = reshape(X2_resampled(7:end), 6, 6);
+
+% Map stable and unstable vectors forward
+S_1 = phi_star_1 * vector_stable_1;
+S_1 = S_1 / norm(S_1);
+U_1 = phi_star_1 * vector_unstable_1;
+U_1 = U_1 / norm(U_1);
+S_2 = phi_star_2 * vector_stable_2;
+S_2 = S_2 / norm(S_2);
+U_2 = phi_star_2 * vector_unstable_2;
+U_2 = U_2 / norm(U_2);
+
+% create perturbation vector
+pert = ones(6, 1) * p('pert');
+
+% Perturb conditions
+XS_left_1 = x_star_1' + S_1 .* pert;
+XS_right_1 = x_star_1' - S_1 .* pert;
+XU_left_1 = x_star_1' + U_1 .* pert;
+XU_right_1 = x_star_1' - U_1 .* pert;
+
+XS_left_2 = x_star_2' + S_2 .* pert;
+XS_right_2 = x_star_2' - S_2 .* pert;
+XU_left_2 = x_star_2' + U_2 .* pert;
+XU_right_2 = x_star_2' - U_2 .* pert;
+
+%% calculate the stable and unstable manifolds
+tf = 6;
+tspan_s = [tf 0];
+tspan_u = [0 tf];
+[~, xs_left_1] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_s, XS_left_1, options_ODE);
+[~, xs_right_1] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_s, XS_right_1, options_ODE);
+[~, xu_left_1] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_u, XU_left_1, options_ODE);
+[~, xu_right_1] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_u, XU_right_1, options_ODE);
+
+[~, xs_left_2] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_s, XS_left_2, options_ODE);
+[~, xs_right_2] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_s, XS_right_2, options_ODE);
+[~, xu_left_2] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_u, XU_left_2, options_ODE);
+[~, xu_right_2] = ode113(@(t,x) fun_cr3bp(t, x, p('mu')), tspan_u, XU_right_2, options_ODE);
+
+% plot stable and unstable manifolds
+figure();
+plot(xs_left_1(:, 1), xs_left_1(:, 2), 'r', 'LineWidth', 2);
+hold on
+plot(xs_right_1(:, 1), xs_right_1(:, 2), 'r', 'LineWidth', 2);
+plot(xu_left_1(:, 1), xu_left_1(:, 2), 'b', 'LineWidth', 2);
+plot(xu_right_1(:, 1), xu_right_1(:, 2), 'b', 'LineWidth', 2);
+plot(xs_left_2(:, 1), xs_left_2(:, 2), 'r', 'LineWidth', 2);
+plot(xs_right_2(:, 1), xs_right_2(:, 2), 'r', 'LineWidth', 2);
+plot(xu_left_2(:, 1), xu_left_2(:, 2), 'b', 'LineWidth', 2);
+plot(xu_right_2(:, 1), xu_right_2(:, 2), 'b', 'LineWidth', 2);
+plot(x_corrected_1(:, 1), x_corrected_1(:, 2), 'k', 'LineWidth', 2);
+plot(x_corrected_2(:, 1), x_corrected_2(:, 2), 'k', 'LineWidth', 2);
+plot(L1(1), L1(2), 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'k');
+plot(L2(1), L2(2), 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'k');
+plot(x_star_1(1), x_star_1(2), 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'm');
+plot(x_star_2(1), x_star_2(2), 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'm');
+plot(1-mu, 0, 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'k');
+xlabel('$x$ [-]', 'Interpreter', 'latex', 'FontSize', 24);
+ylabel('$y$ [-]', 'Interpreter', 'latex', 'FontSize', 24);
+xlim([0.7 1.3]);
+ylim([-0.2, 0.2]);
+axis equal
+grid on
+hold off
+
